ed65d93bc820eb79aa7567cf7c0c0a09130736f1
JackAidley <github@jackaidley.co.uk>
2026-04-01 10:52:41 +0200

    Course Completion Report as of 2025-07-10

--- Course Completion.sql
+++ Course Completion.sql
@@ -1,43 +1,50 @@
 SELECT
     u.firstname "Firstname",
+
     CONCAT('<a target="_blank" href="%%WWWROOT%%/user/profile.php?id=', u.id, '">', u.lastname,'</a>') "Lastname",
+    
     u.email "Email",
+    
     (
         SELECT
             CONCAT(manuser.firstname, ' ', manuser.lastname)
         FROM
             prefix_user manuser WHERE mandata.data <> '' AND manuser.id = mandata.data::bigint
     ) "Manager",
+    
     (
         SELECT d.data
         FROM prefix_user_info_field AS f JOIN prefix_user_info_data AS d ON f.id = d.fieldid
         WHERE f.shortname = 'company' AND d.userid = u.id
     ) "Company",
+    
     u.idnumber "ID Number (SSO)",
+    
     (
         SELECT d.data
         FROM prefix_user_info_field AS f JOIN prefix_user_info_data AS d ON f.id = d.fieldid
         WHERE f.shortname = 'employeenumber' AND d.userid = u.id
     ) "Employee Number",
+    
     cc.name AS "Category",
+    
     CONCAT('<a target="_blank" href="%%WWWROOT%%/course/view.php?id=', c.id, '">', c.shortname,'</a>') "Course",
+    
     course_hours.value as "Course Hours",
+    
     CASE
         WHEN hours_billable.value::int = 1 THEN 'YES'
         ELSE 'NO'
     END as "Hours Billable",
-    to_char(to_timestamp(p.timecompleted),'YYYY-MM-DD') "Date completed",
-    (
-        CASE
-            WHEN p.timestarted = 0 THEN  null
-            ELSE  to_char(to_timestamp(p.timestarted),'YYYY-MM-DD')
-            END
-        ) AS "Start date",
+    
+    to_char(to_timestamp(p.latestcomp),'YYYY-MM-DD') "Date completed",
+    
     (
         SELECT string_agg(e.enrol, ', ')
         FROM prefix_user_enrolments AS ue JOIN prefix_enrol AS e ON ue.enrolid = e.id
         WHERE ue.userid = u.id and e.courseid = c.id
     ) AS "Enrollment Methods",
+    
     (
         SELECT
             d.data
@@ -47,9 +54,10 @@ SELECT
         WHERE
                 f.shortname = 'airtimerole' AND d.userid = u.id
     ) "AT Role"
+
 FROM
-    prefix_course_completions AS p
-        JOIN prefix_course AS c ON p.course = c.id
+    prefix_local_recompletion_cc_cached AS p
+        JOIN prefix_course AS c ON p.courseid = c.id
         JOIN prefix_course_categories cc ON c.category = cc.id
         JOIN prefix_user AS u ON p.userid = u.id
         LEFT JOIN prefix_customfield_data hours_billable ON hours_billable.instanceid = c.id AND hours_billable.fieldid = (SELECT cf.id FROM prefix_customfield_field cf WHERE cf.shortname = 'hours_billable')
@@ -57,15 +65,16 @@ FROM
         LEFT JOIN prefix_user_info_field AS manfield ON manfield.shortname = 'managerid'
         LEFT JOIN prefix_user_info_data AS mandata ON mandata.fieldid = manfield.id AND mandata.userid = u.id
 WHERE
-        c.enablecompletion = 1
-  AND p.timecompleted > 0
-    %%FILTER_SUBCATEGORIES:cc.path%%
+    c.enablecompletion = 1
+
+%%FILTER_SUBCATEGORIES:cc.path%%
 %%FILTER_COURSES:p.course%%
 %%FILTER_SEARCHTEXT_fullname:CONCAT(u.firstname, ' ', u.lastname):~ci%%
 %%FILTER_SQL_cohortid:(SELECT chr.id FROM prefix_cohort AS chr JOIN prefix_cohort_members AS mem ON chr.id = mem.cohortid WHERE chr.visible = 1 AND mem.userid = u.id):in%%
 %%FILTER_SQL_sqltrecords:u.suspended:=%%
-%%FILTER_STARTTIME:p.timecompleted:>=%%
-%%FILTER_ENDTIME:p.timecompleted:<%%
+%%FILTER_STARTTIME:p.latestcomp:>=%%
+%%FILTER_ENDTIME:p.latestcomp:<%%
 %%FILTER_SQL_manager:mandata.data:=%%
+
 ORDER BY
     u.username ASC
