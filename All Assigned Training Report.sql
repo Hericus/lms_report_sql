1b7ce5d42d91918c652a2455bf051d2fb1454b0e
JackAidley <github@jackaidley.co.uk>
2026-04-01 09:39:12 +0200

    All assigned training report.sql as per 2025-08-22

--- All Assigned Training Report.sql
+++ All Assigned Training Report.sql
@@ -12,7 +12,7 @@ SELECT
         WHEN result.status = 1 THEN 'Completed'
         WHEN result.status = 2 THEN '<span class="text-warning"><strong>Coming due</strong></span>'
         WHEN result.status = 3 THEN '<span class="text-danger"><strong>Overdue</strong></span>'
-        END  "Status",
+    END  "Status",
     result.duration  "Duration",
     result.expiration  "Expiration Date",
     result.sso  "SSO",
@@ -47,54 +47,59 @@ FROM
             CONCAT('<a href="/course/view.php?id=', c.id, '">', c.fullname, '</a>') "course",
             to_char(to_timestamp(cached.originalcomp), 'YYYY-MM-DD') "original",
             to_char(to_timestamp(cached.latestcomp), 'YYYY-MM-DD') "recent",
+
+            CASE
+            WHEN cached.latestcomp IS NULL THEN
+                (
+                    SELECT
+                        CASE
+                            WHEN cfggrace.value::bigint > 0 AND
+                            ((SELECT MAX(GREATEST(ue2.timecreated, ue2.timestart, ue2.timemodified))
+                            FROM prefix_user_enrolments ue2 JOIN prefix_enrol e2 ON e2.id = ue2.enrolid
+                            WHERE e2.courseid = c.id AND ue2.userid = u.id) + cfggrace.value::bigint > extract(epoch from now())) THEN 2
+                            ELSE 3
+                        END
+                )
+            WHEN cfgenable.value = '1' AND cfgrecompletiondur.value IS NOT NULL THEN
+            (
+                SELECT
+                    CASE
+                        WHEN cached.latestcomp + cached.latestduration < extract(epoch from now()) THEN 3
+                        WHEN cached.latestcomp + cached.latestduration -
+                        cached.latestnotify < extract(epoch from now()) THEN 2
+                        ELSE 1
+                    END
+            )
+            WHEN cached.latestcomp IS NOT NULL THEN 1
+            END "status",
+
+            CASE
+                WHEN cfgenable.value = '1' AND (cached.latestduration = 31449600 OR cached.latestduration = 31622400) THEN 'Annual'
+                WHEN cfgenable.value = '1' AND (cached.latestduration = 62899200 OR cached.latestduration = 63158400) THEN 'Biennial'
+                WHEN cfgenable.value = '1' AND (cached.latestduration = 94348800 OR cached.latestduration = 94694400) THEN 'Triennial'
+            END "duration",
+
             CASE
                 WHEN cached.latestcomp IS NULL THEN
-                    (
-                        SELECT
-                            CASE
-                                WHEN cfggrace.value::bigint > 0 AND
-    ((SELECT MAX(GREATEST(ue2.timecreated, ue2.timestart, ue2.timemodified))
-    FROM prefix_user_enrolments ue2 JOIN prefix_enrol e2 ON e2.id = ue2.enrolid
-    WHERE e2.courseid = c.id AND ue2.userid = u.id) + cfggrace.value::bigint > extract(epoch from now())) THEN 2
-    ELSE 3
-    END
-                    )
+                (
+                SELECT
+                    CASE
+                        WHEN cfggrace.value::bigint > 0
+                        THEN to_char(to_timestamp(
+                        (SELECT MAX(GREATEST(ue2.timecreated, ue2.timestart, ue2.timemodified))
+                        FROM prefix_user_enrolments ue2
+                            JOIN prefix_enrol e2 ON e2.id = ue2.enrolid
+                        WHERE e2.courseid = c.id AND ue2.userid = u.id) + cfggrace.value::bigint), 'YYYY-MM-DD')
+                        ELSE NULL
+                    END
+                )
                 WHEN cfgenable.value = '1' AND cfgrecompletiondur.value IS NOT NULL THEN
-                    (
-                        SELECT
-                            CASE
-                                WHEN cached.latestcomp + cached.latestduration < extract(epoch from now()) THEN 3
-                                WHEN cached.latestcomp + cached.latestduration -
-                                     cached.latestnotify < extract(epoch from now()) THEN 2
-                                ELSE 1
-                                END
-                    )
-                WHEN cached.latestcomp IS NOT NULL THEN 1
-                END "status",
-            CASE
-                WHEN cfgenable.value = '1' AND cached.latestduration = 31449600 THEN 'Annual'
-                WHEN cfgenable.value = '1' AND cached.latestduration = 62899200 THEN 'Biennial'
-                WHEN cfgenable.value = '1' AND cached.latestduration = 94348800 THEN 'Triennial'
-                END "duration",
-            CASE
-                WHEN cached.latestcomp IS NULL THEN
-                    (
-                        SELECT
-                            CASE
-                                WHEN cfggrace.value::bigint > 0
-    THEN to_char(to_timestamp(
-    (SELECT MAX(GREATEST(ue2.timecreated, ue2.timestart, ue2.timemodified))
-    FROM prefix_user_enrolments ue2
-    JOIN prefix_enrol e2 ON e2.id = ue2.enrolid
-    WHERE e2.courseid = c.id AND ue2.userid = u.id) + cfggrace.value::bigint), 'YYYY-MM-DD')
-    ELSE NULL
-    END
-                    )
-                WHEN cfgenable.value = '1' AND cfgrecompletiondur.value IS NOT NULL THEN
-                    to_char(to_timestamp(cached.latestcomp + cached.latestduration), 'YYYY-MM-DD')
+                to_char(to_timestamp(cached.latestcomp + cached.latestduration), 'YYYY-MM-DD')
                 ELSE NULL
-                END "expiration",
+            END "expiration",
+
             u.idnumber  "sso",
+
             (
                 SELECT
                     d.data
@@ -103,6 +108,7 @@ FROM
                 WHERE
                         f.shortname = 'lobname' AND d.userid = u.id
             )  "lob",
+
             (
                 SELECT
                     d.data
@@ -111,12 +117,14 @@ FROM
                 WHERE
                         f.shortname = 'region' AND d.userid = u.id
             )  "region",
+            
             (
                 SELECT
                     CONCAT(manuser.firstname, ' ', manuser.lastname)
                 FROM
                     prefix_user manuser WHERE mandata.data <> '' AND manuser.id = mandata.data::bigint
             ) "manager",
+            
             (
                 SELECT
                     d.data
@@ -138,6 +146,7 @@ FROM
                 WHERE
                         f.shortname = 'job_status' AND d.userid = u.id
             ) "jobstatus",
+            
             (
                 SELECT
                     d.data
@@ -147,6 +156,7 @@ FROM
                 WHERE
                         f.shortname = 'airtimerole' AND d.userid = u.id
             ) "airtimerole",
+            
             (
                 SELECT
                     d.data
@@ -156,6 +166,7 @@ FROM
                 WHERE
                         f.shortname = 'active_sup' AND d.userid = u.id
             ) "activesup",
+            
             (
                 SELECT
                     d.data
@@ -171,7 +182,7 @@ FROM
                 JOIN prefix_course AS c ON c.id = e.courseid
                 JOIN prefix_course_categories cc ON c.category = cc.id
                 JOIN prefix_user AS u ON u.id = ue.userid
-                JOIN prefix_cohort cohort ON cohort.id = e.customint1
+                LEFT JOIN prefix_cohort cohort ON cohort.id = e.customint1
                 LEFT JOIN prefix_local_recompletion_cc_cached cached ON cached.userid = u.id AND cached.courseid = c.id
                 LEFT JOIN prefix_local_recompletion_config cfgenable ON cfgenable.course = c.id AND cfgenable.name = 'enable'
                 LEFT JOIN prefix_local_recompletion_config cfgrecompletiondur ON cfgrecompletiondur.course = c.id AND cfgrecompletiondur.name = 'recompletionduration'
@@ -179,17 +190,19 @@ FROM
                 LEFT JOIN prefix_user_info_field AS manfield ON manfield.shortname = 'managerid'
                 LEFT JOIN prefix_user_info_data AS mandata ON mandata.fieldid = manfield.id AND mandata.userid = u.id
         WHERE
-                e.enrol = 'cohort' AND e.status = 0 AND ue.status = 0
-          AND c.enablecompletion = 1
-          AND c.visible = 1
-          AND u.id = %%USERID%%
+                (e.enrol = 'cohort' OR e.enrol = 'manual') 
+            AND e.status = 0 
+            AND ue.status = 0
+            AND c.enablecompletion = 1
+            AND c.visible = 1
+            AND u.id = %%USERID%%
 
 
-            %%FILTER_SUBCATEGORIES:cc.path%%
-%%FILTER_COURSES:c.id%%
+        %%FILTER_SUBCATEGORIES:cc.path%%
+        %%FILTER_COURSES:c.id%%
         ORDER BY u.lastname ASC
 
     ) AS result
 WHERE 1 = 1
 
-    %%FILTER_SQL_status:result.status:=%%
+%%FILTER_SQL_status:result.status:=%%
